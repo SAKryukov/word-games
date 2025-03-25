5724a62befe9e6fe681eee517d643147b969f55e
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2025-03-25 01:04:02 -0400

    Code: words/all: dictionaries reworked, added words, recreated indexing, compacted, refined maintenance code

--- code/Dictionaries/dictionaries.js
+++ code/Dictionaries/dictionaries.js
@@ -11,6 +11,13 @@ const dictionaries = { "English": English, "Russian": Russian };
 
 const performAdHocDictionaryMainenance = () => {
 
+    const compactMode = true;
+
+    const spacer = compactMode ? null : "  ";
+
+    const wrap = (languageName, json) =>
+       `"use strict";\nconst ${languageName} =\n${json};`;
+    
     const dictionaryMaintenance = (languageName, newWords) => {
         for (let word of newWords)
             dictionaries[languageName].alphabetical[word] = word.length;
@@ -22,10 +29,25 @@ const performAdHocDictionaryMainenance = () => {
         dictionaries[languageName].alphabetical = {};
         for (let word of array)
             dictionaries[languageName].alphabetical[word] = word.length;
-        const json = JSON.stringify(dictionaries[languageName], null, "  ");
-        navigator.clipboard.writeText(json);
+        // indexing:
+        for (let index = 0; index < array.length; ++index) {
+            const word = array[index];
+            if (dictionaries[languageName].indexedByLength[word.length] == null)
+                dictionaries[languageName].indexedByLength[word.length] = [];
+            dictionaries[languageName].indexedByLength[word.length].push(index);
+        } //loop
+        //
+        const json = JSON.stringify(dictionaries[languageName], null, spacer);
+        navigator.clipboard.writeText(wrap(languageName, json));
     }; //dictionaryMaintenance
 
-    dictionaryMaintenance("English", []);
+    const transform = languageName => {
+        const json = JSON.stringify(dictionaries[languageName], null, spacer);
+        navigator.clipboard.writeText(wrap(languageName, json));
+    }; //transform
+
+    //dictionaryMaintenance("Russian", ["пита","кепи","шаурма","шаверма","оммаж","фуа","маракуйя","гранадилла","арахнофобия","арахнофоб","неолиберал","неолиберализм","неоконсерватор","неоконсерватизм","терапсид","горгонопс","алкен","алкин","криогений","тоний","мира","пескоструйщица","катык","сузьма","курт","кевлар","сарт","килт","икта","спарка","парка"]);
+    //dictionaryMaintenance("English", []);
+    //transform("English");
 
 }; //performAdHocDictionaryMainenance

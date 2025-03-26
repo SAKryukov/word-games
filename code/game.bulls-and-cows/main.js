b47ce4acdec44a320a1fda0c0a0065ab13ec963e
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2025-03-26 10:05:03 -0400

    Code: restructured, added: added the game bulls-and-cows

---
+++ code/game.bulls-and-cows/main.js
@@ -0,0 +1,12 @@
+/*
+  Word games
+  Copyright (c) 2025 by Sergey A Kryukov
+  http://www.SAKryukov.org
+  https://github.com/SAKryukov/word-games
+*/
+
+"use strict";
+
+window.onload = () => {
+
+}; //window.onload

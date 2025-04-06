4e767ea2f4f26d6004f5335005e03ba5a085ad51
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2025-04-06 14:58:10 -0400

    Code: refined dictionary system definition

--- code/Dictionaries/definition-set.js
+++ code/Dictionaries/definition-set.js
@@ -7,38 +7,46 @@
 
 "use strict";
 
-const dictionary = namespaces.create({
-    
-    definitionSet: {
+const dictionary = namespaces.create({    
+    definitionSet: (() => {
 
-        invalidDictionary: {
-            dictionary: "Dictionary is not defined",             
-            name: "Dictionary should have the property languageName",
-            property: (languageName, propertyName) =>
-                `Dictionary "${languageName}" should have the property ${propertyName}`,
-            alphabetical: languageName =>
-                `Dictionary "${languageName}" should have the property alphabetical, array or words`,
-            quotesLength: languageName =>
-                `For the dictionary "${languageName}", the property characterRepertoire.quotes should have at least one pair of quotation marks`,
-        }, //invalidDictionary
-        propertyName: {
-            culture: "culture",
-            alphabetical: "alphabetical",
-            indexedByLength: "indexedByLength",
-            characterRepertoire: "characterRepertoire",
-            characters: "characters",
-            letters: "letters",
-            vowels: "vowels",
-            consonants: "consonants",
-            punctuation: "punctuation",
-            blankSpace: "blankSpace",
-            quotes: "quotes",
-        },
-        alertText: (message, script) =>
-            `${message}\n\nFix the script\n${script}`,
-        alertTextUnknownScript: message =>
-            `${message}`,
+        const definitionSet = {
+            invalidDictionary: {
+                dictionary: "Dictionary is not defined",             
+                name: "Dictionary should have the property languageName",
+                property: (languageName, propertyName) =>
+                    `Dictionary "${languageName}" should have the property ${propertyName}`,
+                alphabetical: languageName =>
+                    `Dictionary "${languageName}" should have the property alphabetical, array or words`,
+                quotesLength: languageName =>
+                    `For the dictionary "${languageName}", the property characterRepertoire.quotes should have at least one pair of quotation marks`,
+            }, //invalidDictionary
+            propertyName: {
+                culture: 0,
+                alphabetical: 0,
+                indexedByLength: 0,
+                characterRepertoire: 0,
+                    characters: 0,
+                    letters: 0,
+                    vowels: 0,
+                    consonants: 0,
+                    punctuation: 0,
+                    blankSpace: 0,
+                    quotes: 0,
+            }, //propertyName
+            alertText: (message, script) =>
+                `${message}\n\nFix the script\n${script}`,
+            alertTextUnknownScript: message =>
+                `${message}`,    
+        }; //definitionSet
 
-    }, //definitionSet
+        const rebuildNames = instance => {
+            for (let index in instance)
+                instance[index] = index;
+        }; //rebuildNames
+        rebuildNames(definitionSet.propertyName);
 
+        return definitionSet;
+
+    })(),
 }); //dictionary.definitionSet
